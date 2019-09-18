792e5ab03c0e1498cce9e6ddf3c15d096e42974b
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-18 00:28:50 -0400

    cursor

--- main.js
+++ main.js
@@ -9,6 +9,14 @@ function startMap() {
             center: ironhackBCN
         }
     );
+    const myMarker = new google.maps.Marker({
+        position: {
+            lat: 41.3977381,
+            lng: 2.190471916
+        },
+        map: map,
+        title: "I'm here"
+    });
 }
 
 startMap();
